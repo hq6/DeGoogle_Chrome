85e88aab1fb8022e218f65dad5e860ff83dfebdb
Henry Qin <hqin2011@gmail.com>
2016-01-02 06:30:29 -0800

    Modified to only run on Google sites

--- content.js
+++ content.js
@@ -1,7 +1,13 @@
-var actualCode = ['setTimeout(function() {',
-'    rwt=function(){}; ',
-'}, 1000); '].join('\n');
-var script = document.createElement('script');
-script.textContent = actualCode;
-(document.head||document.documentElement).appendChild(script);
-script.parentNode.removeChild(script);
+function main() {
+    var actualCode = ['setTimeout(function() {',
+    '    rwt=function(){}; ',
+    '}, 1000); '].join('\n');
+    var script = document.createElement('script');
+    script.textContent = actualCode;
+    (document.head||document.documentElement).appendChild(script);
+    script.parentNode.removeChild(script);
+}
+
+if (window.location.href.match(/\.google\./)) {
+    main();
+}
